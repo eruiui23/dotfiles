fe6a265fc0ce73ebaa592880b154deb77267c9e4
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-02-12 18:19:25 +0700

    update kitty, bar

--- scripts/start.sh
+++ scripts/start.sh
@@ -26,22 +26,13 @@
 #  obsidian &
 #  i3-msg restart
 
-# 1. Give the system and GPU time to fully initialize the X session
 sleep 3
 
-# 2. Fix the compositor (Ensure picom is running first without the broken flag)
-# If you launch picom here, use: picom -b --backend glx &
-# sleep 1
-
-# 3. Launch background/lighter apps first
 spotify &
 sleep 1
 nemo &
-sleep 1
-
-# 4. Launch heavy GPU apps one at a time
-# WhatsApp (Firefox WebApp)
-sh -c 'XAPP_FORCE_GTKWINDOW_ICON="whatsapp" firefox --class WebApp-whatsapp2493 --name WebApp-whatsapp2493 --profile /home/yucandy16/.local/share/ice/firefox/whatsapp2493 --no-remote "https://web.whatsapp.com/"' &
+# sleep 1
+# sh -c 'XAPP_FORCE_GTKWINDOW_ICON="whatsapp" firefox --class WebApp-WhatsApp5589 --name WebApp-WhatsApp5589 --profile /home/yucandy16/.local/share/ice/firefox/WhatsApp5589 --no-remote "https://web.whatsapp.com/"' &
 sleep 2
 
 # Zen Browser
