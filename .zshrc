89a171d9683cfd5e0060dbbc0cc80129f18798ca
eruiui23 <77096735+eruiui23@users.noreply.github.com>
2026-08-16 13:13:27 +0700

    remove autostart app, fixed nemo terminal not working

--- .zshrc
+++ .zshrc
@@ -158,6 +158,14 @@ npx() { lazy_nvm; npx "$@"; }
 nvm() { lazy_nvm; nvm "$@"; }
 
 ## TMUX STUFF
+#
+# In your ~/.bashrc / ~/.zshrc:
+# Don't auto-launch tmux if running inside Nemo's embedded terminal
+if [ -z "$TMUX" ] && [[ "$(ps -p $PPID -o comm=)" != "nemo" ]]; then
+    # Your tmux launch command here
+    # tmux new-session -A -s main
+    tmux
+fi
 
 # # Automatically open or attach to tmux
 # if command -v tmux &> /dev/null && [ -n "$PS1" ] && [[ -z "$TMUX" ]]; then
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-#  #  i3-msg "workspace 1; append_layout ~/.config/i3/layout.json"
-#
-kitty --hold /home/yucandy16/.local/bin/sakurafetch --fetch --theme nara &
-
-# kitty tmux new-session -s 0 "/home/yucandy16/.local/bin/sakurafetch --fetch --theme nara; exec \$SHELL" &
-
-#  # (kitty zsh -c "neofetch" &)
-#
-#  # sleep 0.3
-#  # (kitty bash -c "unimatrix -l k -s 95; exec zsh" &)
-#  # sleep 0.3
-#  # (kitty bash -c "peaclock; exec zsh" &)
-#  # sleep 0.3
-#  # (kitty bash -c "cava; exec zsh" &)
-#
+# kitty --hold /home/yucandy16/.local/bin/sakurafetch --fetch --theme nara &
+kitty tmux new-session "/home/yucandy16/.local/bin/sakurafetch --fetch --theme nara; exec \$SHELL" &
 
 sleep 3
 # ~/.config/i3/anti-steal-focus.py &
@@ -22,18 +9,14 @@ sleep 3
 ~/scripts/change_wallpaper.sh &
 
 ~/scripts/temp_control.sh &
-~/.local/share/zen/zen &
-sleep 1
-nemo &
+# APP Startup
+# ~/.local/share/zen/zen &
 # sleep 1
-# sh -c 'XAPP_FORCE_GTKWINDOW_ICON="whatsapp" firefox --class WebApp-WhatsApp5233 --name WebApp-WhatsApp5233 --profile /home/yucandy16/.local/share/ice/firefox/WhatsApp5233 --no-remote "https://web.whatsapp.com/"' &
-sleep 1
-spotify &
-# Flatpaks and others
-flatpak run com.discordapp.Discord &
-sleep 1
-obsidian &
-anki &
-# sleep 3
-# i3-msg workspace 1
-# i3-msg '[urgent="latest"] focus'
+# nemo &
+# sleep 1
+# spotify &
+# # Flatpaks and others
+# flatpak run com.discordapp.Discord &
+# sleep 1
+# obsidian &
+# anki &
